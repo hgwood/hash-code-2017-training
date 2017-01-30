ff22ea94b39e3d72e4af007d18da2bca0631c493
Hugo Wood <hx4.5@free.fr>
2017-01-30 22:54:39 +0100

    chore: scripted code archive creation

---
+++ zip.js
@@ -0,0 +1,30 @@
+const _ = require('lodash')
+const fs = require('fs')
+const archiver = require('archiver')
+const glob = require('glob')
+const path = require('path')
+const exec = require('child_process').execSync
+
+try {
+  exec("git add *.js && git commit -m 'tayo!'", {encoding: 'utf8'})
+} catch (err) {}
+const sha1 = exec('git rev-parse HEAD', {encoding: 'utf8'}).trim()
+const date = new Date().toISOString().replace(/:/g, '-')
+const dest = `./.builds/submission-sources-${date}-${sha1}.zip`
+
+try {
+  fs.mkdirSync(path.dirname(dest))
+} catch (err) {
+  if (err.code !== 'EEXIST') throw err
+}
+
+const files = glob.sync('!(node_modules)', {})
+const archive = archiver('zip')
+_.each(files, file => archive.file(file, {name: path.basename(file)}))
+archive.finalize().pipe(fs.createWriteStream(dest))
+
+if (process.argv[2] === 'upload') {
+  require('./upload')('/out.txt', '/' + dest)
+}
+
+console.log(`wrote ${files.length} files to ${dest}`)
