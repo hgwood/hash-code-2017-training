8b5e833b781fd9b4790ade81ae75943c809580db
Hugo Wood <hx4.5@free.fr>
2017-02-15 20:34:26 +0100

    feat: read

--- read.js
+++ read.js
@@ -6,11 +6,21 @@ const jolicitron = require('jolicitron')
 module.exports = function read (filePath) {
   const textFromInputFile = fs.readFileSync(filePath, 'utf8')
   debug(`read ${textFromInputFile.length} chars from ${filePath}`)
-  const {parsedValue, remaining} = parse(textFromInputFile)
-  assert(!remaining.trim(), 'Text from input file not completely parsed. The parser is not correct.')
-  return parsedValue
+  const [header, ...pizza] = textFromInputFile.split('\n')
+  const [nrows, ncolumns, minIngredient, maxSliceSize] = header.split(' ').map(i => Number(i))
+  const result = {
+    nrows,
+    ncolumns,
+    minIngredient,
+    maxSliceSize,
+    pizza: pizza.map(row => row.split())
+  }
+  assert.equal(typeof nrows, 'number', 'expected nrows to be a number')
+  assert.equal(typeof ncolumns, 'number', 'expected ncolumns to be a number')
+  assert.equal(typeof minIngredient, 'number', 'expected minIngredient to be a number')
+  assert.equal(typeof maxSliceSize, 'number', 'expected maxSliceSize to be a number')
+  assert.equal(typeof result.pizza, 'object', 'expected pizza to be an object')
+  assert.equal(typeof result.pizza[0], 'object', 'expected pizza row to be an object')
+  assert.equal(typeof result.pizza[0][0], 'string', 'expected pizza cell to be a string')
+  return result
 }
-
-const parse = module.exports.parse = jolicitron((save, n) => [
-  // TODO: insert parser definition here
-])
