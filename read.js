4c6d5662ad23c3851f5d14c99954ed751e1d544b
Hugo Wood <hx4.5@free.fr>
2017-02-15 20:47:24 +0100

    fix: read test and read fixes

--- read.js
+++ read.js
@@ -1,23 +1,26 @@
 const assert = require('assert')
 const fs = require('fs')
 const debug = require('debug')('read')
-const jolicitron = require('jolicitron')
 
 module.exports = function read (filePath) {
   const textFromInputFile = fs.readFileSync(filePath, 'utf8')
   debug(`read ${textFromInputFile.length} chars from ${filePath}`)
-  const [header, ...pizza] = textFromInputFile.split('\n')
-  const [nrows, ncolumns, minIngredient, maxSliceSize] = header.split(' ').map(i => Number(i))
+  return module.exports.parse(textFromInputFile)
+}
+
+module.exports.parse = function (textFromInputFile) {
+  const [header, ...pizza] = textFromInputFile.split('\n').filter(line => line)
+  const [nrows, ncolumns, minIngredients, maxSliceSize] = header.trim().split(' ').map(i => Number(i))
   const result = {
     nrows,
     ncolumns,
-    minIngredient,
+    minIngredients,
     maxSliceSize,
-    pizza: pizza.map(row => row.split())
+    pizza: pizza.map(row => row.trim().split(''))
   }
   assert.equal(typeof nrows, 'number', 'expected nrows to be a number')
   assert.equal(typeof ncolumns, 'number', 'expected ncolumns to be a number')
-  assert.equal(typeof minIngredient, 'number', 'expected minIngredient to be a number')
+  assert.equal(typeof minIngredients, 'number', 'expected minIngredients to be a number')
   assert.equal(typeof maxSliceSize, 'number', 'expected maxSliceSize to be a number')
   assert.equal(typeof result.pizza, 'object', 'expected pizza to be an object')
   assert.equal(typeof result.pizza[0], 'object', 'expected pizza row to be an object')
--- read.test.js
+++ read.test.js
@@ -5,9 +5,21 @@ const {parse} = require('./read')
 
 describe('parse', function () {
   it('parses correctly', function () {
-    const textFromInputFile = `` // TODO: insert test input here
+    const textFromInputFile = `
+      1 2 3 4
+      TTTMMM
+      MTTMMT
+    ` // TODO: insert test input here
     assert.deepEqual(parse(textFromInputFile), {
       // TODO: insert expected output here
+      nrows: 1,
+      ncolumns: 2,
+      minIngredients: 3,
+      maxSliceSize: 4,
+      pizza: [
+        ['T', 'T', 'T', 'M', 'M', 'M'],
+        ['M', 'T', 'T', 'M', 'M', 'T']
+      ]
     })
   })
 })
