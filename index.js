0cc04fbf5101d523a00cef9e449b646818723653
Hugo Wood <hx4.5@free.fr>
2017-02-19 13:56:18 +0100

    feat(index): support for globs in args

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict'
 
-process.env.DEBUG = '*'
+process.env.DEBUG = '*' // enables all debug messages
 
 const _ = require('lodash')
+const glob = require('glob')
 const read = require('./read')
 const write = require('./write')
 const solve = require('./solve')
 
-const files = _.slice(process.argv, 2)
+const files = _(process.argv)
+  .slice(2)
+  .flatMap(filename => glob.sync(filename))
+  .value()
 
 if (_.isEmpty(files)) console.warn('No input files given.')
 
