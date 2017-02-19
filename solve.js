75b74d3619be525e3fe34c5c9c41acfd53782294
Hugo Wood <hx4.5@free.fr>
2017-02-19 13:20:27 +0100

    refactor(solve): removed unreachable code

--- solve.js
+++ solve.js
@@ -5,7 +5,6 @@ const transpose = require('./transpose')
 module.exports = function solve (problem) {
   debug('start')
   return findSlicesByCol(problem)
-  debug('end')
 }
 
 function findSlicesByCol (problem) {
